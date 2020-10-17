39e9d5418667711641e4dfd0761a9771bb99d798
JuanCAlvial <juancalvial@gmail.com>
2020-10-17 13:08:28 -0400

    test

--- src/com/syntax/class07/Recap.java
+++ src/com/syntax/class07/Recap.java
@@ -21,7 +21,57 @@ public class Recap {
 				case "May":
 					System.out.println("Spring");
 					break;
+				case "June":
+				case "July":
+				case "August":
+					System.out.println("Summer");
+					break;
+					
+					
 				}
-
+// Not Recommended
+				
+				
+				String timeOfDay="Morning";
+				String day="Saturday";
+				switch(timeOfDay){
+				case"Morning":
+					System.out.println("Good Morning");
+				
+				switch (day){
+				case "Monday":
+					System.out.println("Colleague");
+					break;
+				case "Thursday":
+					System.out.println("Asghar");
+					break;
+				case "Saturday":
+					System.out.println("Class");
+					break;
+				
+				}
+				//if (day.equals("Saturday")) {
+					//System.out.println("Class!");
+				//}else if(day.equals("Monday")) {
+					//System.out.println("Colleague");
+				//}
+				break;
+				case "Noon":
+					System.out.println("Good Afternon");
+					break;
+				case "Evening":
+					System.out.println("Good Evening");
+					break;
+				case "Night":
+					System.out.println("Good Night");
+					break;
+				case "Saturday":
+				System.out.println("Hello Class");
+				
+				default:
+						System.out.println("N/A");
+				
+				
+				
 	}
 }
---
+++ src/com/syntax/class07/WhileLoopIntro.java
@@ -0,0 +1,38 @@
+package com.syntax.class07;
+
+public class WhileLoopIntro {
+
+	public static void main(String[] args) {
+		int time=15;
+		
+		if(time>12) {
+			System.out.println("good day");
+		}
+			System.out.println("-------------------------------WHILE LOOP=---------------------");
+			//   while(time>12) {
+			//   System.out.println("good day");//INFINITE LOOP
+			//   }
+//			while(time>12) {
+	//			System.out.println("good day");//3
+		//		time--;
+			
+		//}
+			int(time>12) {
+				System.out.println("good day");//
+				time++;	
+			}
+			// I want to print numbers from 1 to 50
+			System.out.println("Printing numbers 1 to 50-----------------------");
+			int num=1;
+			while(num<=50) {
+				System.out.println(num);//0,1
+				num++;
+	}
+			System.out.println("printing numbers 10 to 60-------------------");
+			int(a<61){
+				System.out.println(a);
+				a++;
+			}
+	}
+	
+}
