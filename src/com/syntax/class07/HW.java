d6b3bc7f847d1769ec3087428c564cad136dd9f7
JuanCAlvial <juancalvial@gmail.com>
2020-10-17 13:14:11 -0400

    test comment added

--- src/com/syntax/class07/HW.java
+++ src/com/syntax/class07/HW.java
@@ -4,6 +4,7 @@ public class HW {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//test comment 
 		Scanner input=new Scanner (System.in);
 		System.out.println("Please enter two numbers");
 		double num1=input.nextDouble();
